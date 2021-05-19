78a21f6ca51a9bf3be6055d1fc9331524fa465b2
Jin Oh <jintoh613@gmail.com>
2021-05-18 17:14:35 -0700

    added userController

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ PuffDesk is a lightweight helpdesk looking to take away some setup headache.
 
 ## Installation
 
-Dillinger requires [Node.js](https://nodejs.org/) v10+ to run.
+PuffDesk requires [Node.js](https://nodejs.org/) v10+ to run.
 
 Install the dependencies and devDependencies and start the server.
 
@@ -91,4 +91,4 @@ npm build dist --prod
 
 MIT
 
-**Free Software, Hell Yeah!**
+**Free Software!**
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,13 @@
 
-
+// getCohorts -> userController
+// createUser
+// updateuser
+// deleteuser
+// verifyUser
 
 
 // creates a User and posts to db
-ticketController.createUser = (req, res, next) => {
+userController.createUser = (req, res, next) => {
 
   // deconstruct request body from front end 
    const {
@@ -19,21 +23,80 @@ ticketController.createUser = (req, res, next) => {
     ];
    
    // make query string
-   const queryStr = 'INSERT INTO "public"."UserTable"("UserID",
-   "username", 
-   "password",
-   "cohortID", 
-   "usertypeID") ' + 
-   'VALUES ($1, $2, $3, $4) RETURNING *;';
+   const queryStr = 'INSERT INTO "public"."UserTable"("UserID", "username", "password","cohortID", "usertypeID") ' + 
+   'VALUES ($1, $2, $3, $4, $5) RETURNING *;';
     
    // create async db.query
    db.query(queryStr, values) 
       // send to res.locals object
       .then(data => {
-        console.log('Creating ticket: ', data);
-        res.locals.createdTicket = data[0];
+        console.log('Creating user: ', data);
+        res.locals.createdUser = data[0];
         return next();
       })
       // catch error
       .catch(err => next(err));
   };
+
+  // gets all cohorts
+userController.getCohorts = (req, res, next) => {
+
+  // make a query string (SQL query)
+  const queryStr = 'SELECT * FROM "public"."TicketTable" LIMIT 100'
+
+  // make an async query using db.query and pass in query string
+  db.query(queryStr)
+  .then(data => { // data from the database
+      // console.log('DATA', data.rows);
+      // return next
+      console.log('Gettin cohorts: ', data)
+      res.locals.allCohorts = data.rows;
+      return next();
+    })
+  // calling global eror handler
+  .catch(err => next({
+      log: 'userController.getCohorts: ERROR: Invalid or unfound required data on res.locals object - Expected res.locals to be an object.',
+      message: {err: 'userController.getChorts: ERROR: Check server logs for details'}
+  }))
+}// Type JavaScript here and click "Run Code" or press Ctrl + s
+
+
+userController.verifyUser = (req, res, next) => {
+  const values = [req.body.user, req.body.pass];
+  const statement = 'SELECT * FROM users WHERE username = $1 AND password = $2';
+  db.query(statement, values, (err, result) => {
+    if (err) return next(err);
+    if (result.rows.length) {
+      res.locals.user = result.rows[0];
+      return next();
+    } else {
+      return res.json('Invalid username/password!');
+    }
+  });
+};
+
+userController.updateUser = (req, res, next) => {
+  const queryStr = '';
+  db.query(queryStr, values, (err, result) => {
+    if (err) return next(err);
+    if (result.rows.length) {
+      res.locals.user = result.rows[0];
+      return next();
+    } else {
+      return res.json('Invalid username/password!');
+    }
+  });
+};
+
+userController.deleteUser = (req, res, next) => {
+  const queryStr = '';
+  db.query(queryStr, values, (err, result) => {
+    if (err) return next(err);
+    if (result.rows.length) {
+      res.locals.user = result.rows[0];
+      return next();
+    } else {
+      return res.json('Invalid username/password!');
+    }
+  });
+};
