173429e1549e619058eb45440c2562b6f93264bb
Jin Oh <jintoh613@gmail.com>
2021-05-19 19:38:15 -0700

    completed usercontroller.verifyuser
    Co-authored-by: Bruno Portela <13480425+brunoportela@users.noreply.github.com>
    Co-authored-by: Cliff Assuncao <7276145+WizardSource@users.noreply.github.com>
    Co-authored-by: Yoko Kawamoto <7126966+libero-yoko@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const ticketController = require('./controllers/ticketController');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const ticketController = require("./controllers/ticketController");
 // const cors = require('cors');
 
 // const authRouter = require('./routes/authenticate');
 const PORT = 3000;
 const app = express();
 
-
-// const signupRouter = require('./routes/signupRouter');
+const signupRouter = require("./routes/signupRouter");
 // const loginRouter = require('./routes/loginRouter');
-const ticketRouter = require('./routes/ticketRouter');
-// const userController = require('./controllers/userController');
+const ticketRouter = require("./routes/ticketRouter");
+const userController = require("./controllers/userController");
 // const sessionController = require('./controllers/sessionController');
 
 app.use(cookieParser());
@@ -20,26 +19,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Initial Page Request
-app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../index.html'))
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "../index.html"));
 });
 
-app.use(express.static('client'));
+app.use(express.static("client"));
 
-app.get('/categories', ticketController.getCategories, (req, res) => {
-  console.log('res.locals.categories', res.locals.categories);
+app.get("/categories", ticketController.getCategories, (req, res) => {
+  console.log("res.locals.categories", res.locals.categories);
   res.status(200).send(res.locals.categories);
 });
 /**
  * define route handlers
  */
-// app.use('/signup', signupRouter);
+app.use("/signup", signupRouter);
 // app.use('/login', loginRouter);
-app.use('/ticket', ticketRouter);
+app.use("/ticket", ticketRouter);
+
+app.get("/cohort", userController.getCohorts, (req, res) => {
+  res.status(200).send(res.locals.allCohorts);
+});
 
 // populates dashboard ticket information
-// app.get('/dashboard', 
-//   ticketController.getTicketInfo, 
+// app.get('/dashboard',
+//   ticketController.getTicketInfo,
 //   (req, res) => {
 //     res.status(200).send(res.locals.ticketInfo)
 //   }
@@ -53,10 +56,10 @@ app.use('/ticket', ticketRouter);
 // /login ->  validateUser, startSession, setCookie
 // ticket/getCohortList
 // /signup -> createUser, startSession, setCookie
-// /ticket -> getTickets,  
+// /ticket -> getTickets,
 // /ticket/getCategories
-// /ticket/create -> createTicket, 
-// 
+// /ticket/create -> createTicket,
+//
 
 // history:
 // /getTicketsByUserID
@@ -66,31 +69,28 @@ app.use('/ticket', ticketRouter);
 // fellows: can close tickets (action buttons enabled)
 // admins: can close tickets, change usertype, add users, add/delete cohorts
 
-
 /** Error Handling */
 
 // catch-all route handler for any requests to an unknown route
-app.use((req, res) => res.status(404).send('Error 404'));
+app.use((req, res) => res.status(404).send("Error 404"));
 
 // global error handler
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 500,
-    message: { err: 'An error occurred' },
+    message: { err: "An error occurred" },
   };
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-
 /**
  * start server
  */
- app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
 });
 
 module.exports = app;
-
